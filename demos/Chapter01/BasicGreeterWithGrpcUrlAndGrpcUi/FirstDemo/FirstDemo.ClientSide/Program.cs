5e2efb4a23a5dfa858c26d460bc9d01aacd2ab96
Simona Colapicchioni <s.colapicchioni@gmail.com>
2022-05-10 14:47:50 +0200

    First Commit

---
+++ demos/Chapter01/BasicGreeterWithGrpcUrlAndGrpcUi/FirstDemo/FirstDemo.ClientSide/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FirstDemo.ServerSide;
+using Grpc.Net.Client;
+
+Console.WriteLine("Hello, World!");
+
+GrpcChannel channel = GrpcChannel.ForAddress("https://localhost:7056");
+Greeter.GreeterClient client = new Greeter.GreeterClient(channel);
+
+HelloReply? response = await client.SayHelloAsync(new HelloRequest { Name = "Simona" });
+Console.WriteLine(response.Message);
+

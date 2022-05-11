bb5e0a57eca716deb320c1882380ec39a1f1e70b
Simona Colapicchioni <s.colapicchioni@gmail.com>
2022-05-11 14:52:14 +0200

    ClientFactoryIntegration in console app

---
+++ demos/Chapter03/ClientFactoryIntegration/ClientFactoryIntegration.ConsoleClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using ClientFactoryIntegration.ConsoleClient.Services;
+using ClientFactoryIntegration.ServerSide;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using IHost host = CreateHostBuilder(args).Build();
+
+await host.RunAsync();
+
+IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureServices((hostContext, services) => {
+                services.AddHostedService<MyClass>();
+                services.AddGrpcClient<Greeter.GreeterClient>(o => {
+                    o.Address = new Uri(hostContext.Configuration.GetSection("GrpcAddress").Value);
+                });
+            });
---
+++ demos/Chapter03/ClientFactoryIntegration/ClientFactoryIntegration.ConsoleClient/Services/MyClass.cs
@@ -0,0 +1,18 @@
+﻿using ClientFactoryIntegration.ServerSide;
+using Microsoft.Extensions.Hosting;
+
+namespace ClientFactoryIntegration.ConsoleClient.Services;
+internal class MyClass : IHostedService {
+    private readonly Greeter.GreeterClient client;
+
+    public MyClass(Greeter.GreeterClient client) {
+        this.client = client;
+    }
+    public async Task StartAsync(CancellationToken cancellationToken) {
+        Console.WriteLine((await client.SayHelloAsync(new HelloRequest() { Name = "Simona" })).Message);
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken) {
+        throw new NotImplementedException();
+    }
+}
